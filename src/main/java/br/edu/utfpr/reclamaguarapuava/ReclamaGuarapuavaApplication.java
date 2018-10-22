19fb79233edbaa595301827a92de63374e9c752b
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2018-10-22 16:17:58 -0300

    first commit

---
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/ReclamaGuarapuavaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.reclamaguarapuava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamaGuarapuavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamaGuarapuavaApplication.class, args);
+	}
+}
---
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/controller/HelloController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.reclamaguarapuava.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ *
+ * @author ronifabio
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = GET)
+    public String sayHello() {
+        int f = 10;
+        return "Hello World Spring";
+    }
+
+    @RequestMapping(path = {"/java"}, method = GET)
+    public String sayHelloNovo() {
+        return "Alô Mundo Java";
+    }
+
+}
