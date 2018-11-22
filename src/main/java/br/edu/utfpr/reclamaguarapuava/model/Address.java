cfe864503b1f81ca652ecb596ba0f28a637a12ea
Carlos K <carloskotachoamd@hotmail.com>
2018-11-22 01:54:53 -0200

    Address extends EntityApplication

--- src/main/java/br/edu/utfpr/reclamaguarapuava/model/Address.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/model/Address.java
@@ -1,12 +1,9 @@
 package br.edu.utfpr.reclamaguarapuava.model;
 
-import java.io.Serializable;
+import br.edu.utfpr.reclamaguarapuava.util.EntityApplication;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -23,13 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "address")
-public class Address implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Address extends EntityApplication {
 
     @Column(length = 10, nullable = false)
     private Integer number;
