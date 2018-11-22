61d4b74ec9b6ba9ae4d5b3e5f01c330aaab8bbf0
Carlos K <carloskotachoamd@hotmail.com>
2018-11-22 01:27:58 -0200

    State extends EntityApplication

--- src/main/java/br/edu/utfpr/reclamaguarapuava/model/State.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/model/State.java
@@ -1,12 +1,9 @@
 package br.edu.utfpr.reclamaguarapuava.model;
 
-import java.io.Serializable;
+import br.edu.utfpr.reclamaguarapuava.util.EntityApplication;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -18,13 +15,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "state")
-public class State implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class State extends EntityApplication {
 
     @Column(nullable = false, length = 50)
     private String name;
