3b603b9113e1d039bd065a506672ce9f9e4d5873
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2018-12-03 17:42:44 -0200

    Add swagger web configuration

--- src/main/java/br/edu/utfpr/reclamaguarapuava/util/SwaggerConfig.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/util/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    	return new Docket(DocumentationType.SPRING_WEB.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.edu.utfpr"))
                 .paths(PathSelectors.any())
