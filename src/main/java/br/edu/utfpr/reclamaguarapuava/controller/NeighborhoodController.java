39fab8092beada6f4a4b1389b75698f6bb7e21c4
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2018-12-04 11:14:37 -0200

    neighborhood get post put delete and swagger open in security config

--- src/main/java/br/edu/utfpr/reclamaguarapuava/controller/NeighborhoodController.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/controller/NeighborhoodController.java
@@ -1,13 +1,112 @@
 package br.edu.utfpr.reclamaguarapuava.controller;
 
-import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.utfpr.reclamaguarapuava.model.Neighborhood;
+import br.edu.utfpr.reclamaguarapuava.model.dto.NeighborhoodDTO;
+import br.edu.utfpr.reclamaguarapuava.model.service.NeighborhoodService;
+import br.edu.utfpr.reclamaguarapuava.util.Response;
+import br.edu.utfpr.reclamaguarapuava.util.erros.InvalidParamsException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
-@RequestMapping("/api/v1/bairros")
-@Api(value = "Neighborhood Controller")
+@RequestMapping("/api/v1/admin/bairros")
+@Api(value = "Neighborhood", description = "Url para manipular ou buscar bairros")
 public class NeighborhoodController {
 
+	private final NeighborhoodService neighborhoodService;
+
+	private static final Logger log = LoggerFactory.getLogger(NeighborhoodController.class);
+
+	@Autowired
+	public NeighborhoodController(NeighborhoodService neighborhoodService) {
+		this.neighborhoodService = neighborhoodService;
+	}
+
+	@GetMapping
+	@ApiOperation(value = "Retorna todas os bairros", notes = "A lista retornada é paginada")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Quando bem sucedida para todos os bairros"),
+			@ApiResponse(code = 403, message = "Acesso negado"),
+			@ApiResponse(code = 500, message = "Quando a requisição causou um error interno no servidor"), })
+	public ResponseEntity<Page<Neighborhood>> getAll(Pageable pageable) {
+		log.debug("Request GET to '/api/v1/admin/bairros' in process");
+		return new ResponseEntity<>(neighborhoodService.findAll(pageable), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/{id}")
+	@ApiOperation(value = "Retorna um único bairro")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Quando bem sucedida retorna os detalhes de um bairro", response = Neighborhood.class),
+			@ApiResponse(code = 403, message = "Acesso negado"),
+			@ApiResponse(code = 404, message = "Bairro não existe na base de dados"),
+			@ApiResponse(code = 500, message = "Quando a requisição causou um error interno no servidor"), })
+	public ResponseEntity<Response<Neighborhood>> getById(
+			@ApiParam(name = "id", value = "Númeral do tipo long que corresponde ao id do bairro a qual requer detalhes") @PathVariable("id") Long id) {
+		log.debug("Request GET to '/api/v1/admin/bairros/" + id + "' in process");
+		return new ResponseEntity<>(neighborhoodService.findById(id), HttpStatus.OK);
+	}
+
+	@PostMapping
+	@ApiOperation(value = "Cadastra um bairro")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Quando bem sucedida cadastra um novo bairro"),
+			@ApiResponse(code = 403, message = "Acesso negado"),
+			@ApiResponse(code = 400, message = "Houve um erro, a requisição está inválida"),
+			@ApiResponse(code = 422, message = "Houve um erro, a requisição está inválida, existe uma entidade com o mesmo nome"),
+			@ApiResponse(code = 500, message = "Quando a requisição causou um error interno no servidor"), })
+	public ResponseEntity<Response<Neighborhood>> save(@Valid @RequestBody NeighborhoodDTO dto, BindingResult result) {
+		log.debug("Request POST to '/api/v1/admin/bairros' in process");
+		if (result.hasFieldErrors()) {
+			throw new InvalidParamsException("Params invalid", result);
+		}
+		return new ResponseEntity<>(neighborhoodService.save(dto), HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}")
+	@ApiOperation(value = "Atualiza um bairro")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Quando bem sucedida atualiza o bairro"),
+			@ApiResponse(code = 403, message = "Acesso negado"),
+			@ApiResponse(code = 400, message = "Houve um erro, a requisição está inválida"),
+			@ApiResponse(code = 422, message = "Houve um erro de conflito com outra entidade, por exemplo, nome do bairro já existe"),
+			@ApiResponse(code = 500, message = "Quando a requisição causou um error interno no servidor"), })
+	public ResponseEntity<Response<Neighborhood>> update(@PathVariable Long id, @Valid @RequestBody NeighborhoodDTO dto,
+			BindingResult result) {
+		log.debug("Request PUT to '/api/v1/admin/bairros/" + id + "' in process");
+		if (result.hasFieldErrors()) {
+			throw new InvalidParamsException("Params to update invalid", result);
+		}
+		return new ResponseEntity<>(neighborhoodService.update(dto, id), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	@ApiOperation(value = "Deleta um bairro")
+	@ApiResponses(value = { @ApiResponse(code = 204, message = "Quando bem sucedida deleta o bairro"),
+			@ApiResponse(code = 403, message = "Acesso negado"),
+			@ApiResponse(code = 410, message = "Houve um erro, o recurso não existe ou já foi elimidado de forma permanente"),
+			@ApiResponse(code = 500, message = "Quando a requisição causou um error interno no servidor"), })
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		log.debug("Request DELETE to '/api/v1/admin/bairros/" + id + "' in process");
+		neighborhoodService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/br/edu/utfpr/reclamaguarapuava/model/service/NeighborhoodService.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/model/service/NeighborhoodService.java
@@ -1,12 +1,73 @@
 package br.edu.utfpr.reclamaguarapuava.model.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.edu.utfpr.reclamaguarapuava.model.Neighborhood;
+import br.edu.utfpr.reclamaguarapuava.model.dto.NeighborhoodDTO;
 import br.edu.utfpr.reclamaguarapuava.model.repository.NeighborhoodRepository;
+import br.edu.utfpr.reclamaguarapuava.util.Response;
 
 @Service
 public class NeighborhoodService {
+	private final NeighborhoodRepository neighborhoodRepository;
+
+	private static final Logger log = LoggerFactory.getLogger(NeighborhoodService.class);
+
 	@Autowired
-	NeighborhoodRepository neighborhoodRepository;
+	public NeighborhoodService(NeighborhoodRepository neighborhoodRepository) {
+		this.neighborhoodRepository = neighborhoodRepository;
+	}
+
+	@Transactional(readOnly = true)
+	public Page<Neighborhood> findAll(Pageable pageable) {
+		log.debug("executing query find all");
+		return neighborhoodRepository.findAll(pageable);
+	}
+
+	@Transactional
+	public Response<Neighborhood> findById(Long id) {
+		log.debug("executing query find by id: " + id);
+		Neighborhood neighborhood = neighborhoodRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("neighborhood not found by id: " + id));
+		return new Response<>(neighborhood);
+	}
+
+	@Transactional
+	public Optional<Neighborhood> findByName(String name) {
+		return neighborhoodRepository.findByName(name);
+	}
+
+	@Transactional
+	public Response<Neighborhood> save(NeighborhoodDTO neighborhoodDTO) {
+		log.debug("executing statement insert");
+		this.findByName(neighborhoodDTO.getName()).ifPresent(n -> {
+			throw new EntityExistsException("neighborhood with this name: " + n.getName()
+					+ " exist in database whit id: " + n.getId() + " created: " + n.getCreatedAt());
+		});
+		return new Response<>(neighborhoodRepository.save(new Neighborhood(neighborhoodDTO)));
+	}
+
+	@Transactional
+	public Response<Neighborhood> update(NeighborhoodDTO dto, Long id) {
+		log.debug("executing statement alter");
+		Neighborhood neighborhood = neighborhoodRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("neighborhood not found by id: " + dto.getId()));
+		neighborhood.setName(dto.getName());
+		return new Response<>(neighborhoodRepository.save(neighborhood));
+	}
+
+	public void deleteById(Long id) {
+		neighborhoodRepository.deleteById(id);
+	}
 }
