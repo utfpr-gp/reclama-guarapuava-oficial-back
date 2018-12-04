b356890d70e1ce5429732a205025fe9a89b6d416
Carlos K <carloskotachoamd@hotmail.com>
2018-12-04 01:53:37 -0200

    Adicionado: repositório do comentário

--- src/main/java/br/edu/utfpr/reclamaguarapuava/model/repository/CommentRepository.java
+++ src/main/java/br/edu/utfpr/reclamaguarapuava/model/repository/CommentRepository.java
@@ -3,7 +3,12 @@ package br.edu.utfpr.reclamaguarapuava.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.utfpr.reclamaguarapuava.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    Page<Comment> findAllByOccurrenceId(Long id, Pageable pageable);
+
+    Page<Comment> findAllByUserId(Long id, Pageable pageable);
 }
